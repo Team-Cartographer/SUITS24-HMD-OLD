a85464fffd82ecb1c6f02fcd66b5ef65d05e9459
IRawal <52429323+IRawal@users.noreply.github.com>
2024-01-09 19:02:20 -0800

    Force project setup
    Force push project setup to main

--- HMD/Assets/Scenes/scripts/Test.cs
+++ HMD/Assets/Scenes/scripts/Test.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public TSScConnection TSSc;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("A");
+        TSSc = new TSScConnection();
+        TSSc.ConnectToHost("172.24.196.222", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(TSSc.GetTELEMETRYJsonString());
     }
 }
