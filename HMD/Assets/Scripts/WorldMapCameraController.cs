28245990ff0bebace6ec106dd46d436acf75d251
Abdurrahman Raza <abdurrahmanraza.ed@gmail.com>
2024-01-23 16:41:10 -0800

    Got UI to work with hands
    Replaced Unity UI slider and scrollable view with MRTK's equivalents, so now the map works with your hands. MRTK's scrollable view is experimental, though, and it doesn't work very well at the moment, but I don't know if I can do anything about it at the moment

--- HMD/Assets/Scripts/WorldMapCameraController.cs
+++ HMD/Assets/Scripts/WorldMapCameraController.cs
@@ -1,3 +1,4 @@
+using MixedReality.Toolkit.UX;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine.UI;
 public class WorldMapCameraController : MonoBehaviour
 {
     [SerializeField] RectTransform scrollContent;
-    [SerializeField] Slider sizeSlider;
+    [SerializeField] MixedReality.Toolkit.UX.Slider sizeSlider;
     float scrollConstant = 0.006875f;
 
     Camera mapCamera;
@@ -14,17 +15,17 @@ public class WorldMapCameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        sizeSlider.value = 2;
+        sizeSlider.Value = 2;
         mapCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float scrollMultiplier = scrollConstant * sizeSlider.value;
-        float newX = -scrollContent.localPosition.x * scrollMultiplier;
-        float newZ = -scrollContent.localPosition.y * scrollMultiplier;
+        float scrollMultiplier = scrollConstant * sizeSlider.Value;
+        float newX = -scrollContent.anchoredPosition.x * scrollMultiplier;
+        float newZ = -scrollContent.anchoredPosition.y * scrollMultiplier;
         transform.position = new Vector3(newX, transform.position.y, newZ);
-        mapCamera.orthographicSize = sizeSlider.value;
+        mapCamera.orthographicSize = sizeSlider.Value;
     }
 }
