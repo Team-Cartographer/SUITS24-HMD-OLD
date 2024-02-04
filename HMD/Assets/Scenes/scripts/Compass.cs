72152364dcd4345dee94c95d457f9a4995cafdec
IRawal <52429323+IRawal@users.noreply.github.com>
2024-02-03 16:10:16 -0800

    Removed junk

--- HMD/Assets/Scenes/scripts/Compass.cs
+++ HMD/Assets/Scenes/scripts/Compass.cs
@@ -14,8 +14,6 @@ public class Compass : MonoBehaviour
 {
     public RawImage CompassImage;
 
-    public float heading = 360;
-
     // Start is called before the first frame update
     public ConnectionHandler connectionHandler;
     void Start()
@@ -39,7 +37,7 @@ public class Compass : MonoBehaviour
             JObject jo = JObject.Parse(IMUstring);
             float tssHeading = jo["imu"]["eva1"]["heading"].ToObject<float>();
 
-            cameraOffset = cameraOffset - tssHeading;
+            cameraOffset = cameraYaw - tssHeading;
         }
         
         // Update with tss server/prevent desync
@@ -47,27 +45,5 @@ public class Compass : MonoBehaviour
 
         // Rotate image
         CompassImage.uvRect = new Rect(cameraYaw / 360, 0, 1, 1);
-    }
-    public static string Strip(string S)
-    {
-        var arr = new char[S.Length];
-        var cnt = 0;
-        for (int i = 0; i < S.Length; i++)
-        {
-            switch (S[i])
-            {
-                case ' ':
-                case '\r':
-                case '\n':
-                    break;
-
-                default:
-                    arr[cnt] = S[i];
-                    cnt++;
-                    break;
-            }
-        }
-
-        return new string(arr, 0, cnt);
     }
 }
