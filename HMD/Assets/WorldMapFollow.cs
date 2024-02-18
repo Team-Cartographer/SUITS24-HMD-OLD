ad187fb041449cfa82d823a1d687425c78e4a0c9
John Bouyer <jbouyer151@gmail.com>
2024-02-18 11:04:46 -0800

    changed the font to protect abhi's delicate eyes

--- HMD/Assets/WorldMapFollow.cs
+++ HMD/Assets/WorldMapFollow.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class WorldMapFollow : MonoBehaviour
 {
-    LazyFollow lazyFollow;
     // Start is called before the first frame update
     void Start()
     {
-        lazyFollow = GetComponent<LazyFollow>();
+
     }
 
     // Update is called once per frame
